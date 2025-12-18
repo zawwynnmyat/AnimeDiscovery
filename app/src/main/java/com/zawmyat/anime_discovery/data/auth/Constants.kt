14c37892ed9660fd38d35e85e2de645d000cd239
Zaw Wynn Myat <94133457+zawwynnmyat@users.noreply.github.com>
2025-12-18 19:53:13 +0630

    Update Constants.kt

--- app/src/main/java/com/zawmyat/anime_discovery/data/auth/Constants.kt
+++ app/src/main/java/com/zawmyat/anime_discovery/data/auth/Constants.kt
@@ -2,7 +2,7 @@ package com.zawmyat.anime_discovery.data.auth
 
 object Constants {
     val BASE_URL = "https://anilist.co/api/v2/"
-    val CLIENT_ID = "19983"
-    val CLIENT_SECRET = "p7yJIyHb2PChvDuyVV3lsdzxLNYRxi6dSXaNxU3w"
-    val REDIRECT_URL = "animediscovery://callback"
+    val CLIENT_ID = "YOUR_CLIENT_ID"
+    val CLIENT_SECRET = "YOUR_CLIENT_SECRET"
+    val REDIRECT_URL = "YOUR_REDIRECT_URL"
 }
